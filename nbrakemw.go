33f9ac866d8312f511cae64be9d345ae685df73b
Ali Hamidi <ahamidi@me.com>
2015-08-18 15:05:22 -0700

    update

--- nbrakemw.go
+++ nbrakemw.go
@@ -36,15 +36,11 @@ func (a *AirBraker) ServeHTTP(rw http.ResponseWriter,
 
 	var n *gobrake.Notice
 	if res.Status() >= 400 && res.Status() < 600 {
-		n := gobrake.NewNotice(errors.New(http.StatusText(res.Status())), r, 1)
+		n = gobrake.NewNotice(errors.New(http.StatusText(res.Status())), r, 1)
 		n.Context["environment"] = a.environment
 		if req != nil {
 			n.Context["uri"] = req.RequestURI
 			n.Context["method"] = req.Method
-		}
-		_, err := a.Notifier.SendNotice(n)
-		if err != nil {
-			log.Println("Airbraker Error:", err.Error())
 		}
 	}
 
