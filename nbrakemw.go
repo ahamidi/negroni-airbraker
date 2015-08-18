8dc53b1bbba35f2ccd250a5b337674d1bef74c41
Ali Hamidi <ahamidi@me.com>
2015-08-18 15:42:36 -0700

    wip

--- nbrakemw.go
+++ nbrakemw.go
@@ -30,26 +30,24 @@ func (a *AirBraker) ServeHTTP(rw http.ResponseWriter,
 	r *http.Request,
 	next http.HandlerFunc) {
 
-	// Check return code
-	res := rw.(negroni.ResponseWriter)
-	req := r
+	defer func(rw http.ResponseWriter, r *http.Request) {
+		// Check return code
+		res := rw.(negroni.ResponseWriter)
+		req := r
 
-	var n *gobrake.Notice
-	if res.Status() >= 400 && res.Status() < 600 {
-		n = gobrake.NewNotice(errors.New(http.StatusText(res.Status())), r, 1)
-		n.Context["environment"] = a.environment
-		if req != nil {
-			n.Context["uri"] = req.RequestURI
-			n.Context["method"] = req.Method
+		if res.Status() >= 400 && res.Status() < 600 {
+			n := gobrake.NewNotice(errors.New(http.StatusText(res.Status())), r, 1)
+			n.Context["environment"] = a.environment
+			if req != nil {
+				n.Context["uri"] = req.RequestURI
+				n.Context["method"] = req.Method
+			}
+			_, err := a.Notifier.SendNotice(n)
+			if err != nil {
+				log.Println("Airbraker Error:", err.Error())
+			}
 		}
-	}
+	}(rw, r)
 
 	next(rw, r)
-
-	defer a.Notifier.Flush()
-	_, err := a.Notifier.SendNotice(n)
-	if err != nil {
-		log.Println("Airbraker Error:", err.Error())
-	}
-
 }
